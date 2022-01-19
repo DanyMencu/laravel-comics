922d6dcec793284d888a4bdd83d4487dc459673c
DanyMencu <dany69men@gmail.com>
2022-01-19 18:26:51 +0100

    Little correction

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,8 +1,10 @@
     <header>
         <div class="container nav">
-            <a href="/">
-                <img src="{{ asset('images/dc-logo.png') }}" alt="Dc logo">
-            </a>
+            <div class="logo-container">
+                <a href="{{ route('home') }}">
+                    <img src="{{ asset('images/dc-logo.png') }}" alt="Dc logo">
+                </a>
+            </div>
             <ul class="nav">
                 <li>
                     <a href="{{ route('home') }}"
